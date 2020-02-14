f82f92a8e526105404df020a82edc96a4311f033
harshit <harshit.chawla@soprasteria.com>
2020-02-14 11:54:47 +0530

    BLOG LIST WITHJSON & PREFETCHING

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,12 +1,19 @@
 import { useRouter } from 'next/router'
+import posts from '../../posts.json'
 
 export default () => {
   const router = useRouter()
 
+  const post = posts[router.query.id]
+  if (!post) return (<>
+    <h1>Blog post</h1>
+    <p>Post id: {router.query.id}</p>
+  </>)
+  
   return (
     <>
-      <h1>Blog post</h1>
-      <p>Post id: {router.query.id}</p>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
     </>
   )
 }
